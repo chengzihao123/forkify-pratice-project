872cfab9de3c35389a57b90f5b0a8ec2e047a2eb
chengzihao123 <czh152103@gmail.com>
2023-01-20 10:07:03 +0800

    first commit 200123

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1fc2c7db-3c0f-4e4d-a5f1-cf617dcede6b';
+export const MODAL_CLOSE_SEC = 2.5;
